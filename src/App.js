f48e50889a54126d8da439c4667f2ca311cabc97
JuampiRibero <juampiribero@gmail.com>
2021-12-11 12:24:29 -0300

    cambios menores

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './container/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
-    <div className="App">
+    <>
       <NavBar />
       <ItemListContainer />
-    </div>
+    </>
   );
 }
 
