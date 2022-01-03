8105aa62a2e16468ae731f37b43cb4a13849d2a1
JuampiRibero <juampiribero@gmail.com>
2022-01-03 18:55:07 -0300

    agrego cambios a las cards

--- src/mocks/index.jsx
+++ src/mocks/index.jsx
@@ -63,6 +63,62 @@ const Items = [
         precio: 980,
         descripcion: 'Color rojo violáceo intenso. De complejos aromas a frutas rojas, con delicados toques de madera. Suave en boca, equilibrado, con notas de ciruelas y guindas.'
     },
+    {
+        id: '9',
+        img: 'https://i.ibb.co/KL3m3JX/la-linda-cabernet-sauvignon.jpg',
+        nombre: 'LA LINDA CABERNET SAUVIGNON',
+        bodega: 'luigi bosca',
+        precio: 950,
+        descripcion: 'De color rojo granate con tintes negruzcos. Se sienten aromas a blackberries, pimienta negra y mora. En boca es persistente, intenso, especiado, de taninos jóvenes resultantes de sus 3 meses de añejamiento en roble francés y americano.'
+    },
+    {
+        id: '10',
+        img: 'https://i.ibb.co/BTs32WR/luigi-bosca-cabernet-sauvignon.jpg',
+        nombre: 'LUIGI BOSCA CABERNET SAUVIGNON',
+        bodega: 'luigi bosca',
+        precio: 1700,
+        descripcion: 'Rojo rubí, brillante, limpio. Fresco, frutado aterciopelado. Aromas de frutas rojas y negras, violetas y chocolate. De gran estructura y cuerpo, un vino de carácter y exquisita elegancia.'
+    },
+    {
+        id: '11',
+        img: 'https://i.ibb.co/7QxLDfK/las-perdices-cabernet-sauvignon.jpg',
+        nombre: 'LAS PERDICES CABERNET SAUVIGNON',
+        bodega: 'las perdices',
+        precio: 980,
+        descripcion: 'Color rojo rubí intenso. Aromas de cerezas con sutiles notas de pimienta y suaves aportes de la madera. Suave en boca, balanceado, con aromas a cerezas y cassis.'
+    },
+    {
+        id: '12',
+        img: 'https://i.ibb.co/d7szqQT/finca-gabriel-syrah.jpg',
+        nombre: 'FINCA GABRIEL SYRAH',
+        bodega: 'jorge rubio',
+        precio: 638,
+        descripcion: 'Color rojo oscuro con tonos violáceos muy intensos. Aromas de zarzamora y frambuesa. Un vino con mucho cuerpo, textura aterciopelada y un final en boca equilibrado.'
+    },
+    {
+        id: '13',
+        img: 'https://i.ibb.co/vjk9vGJ/santa-julia-chenin-dulce-natural.jpg',
+        nombre: 'SANTA JULIA CHENIN DULCE NATURAL',
+        bodega: 'santa julia',
+        precio: 530,
+        descripcion: 'Color amarillo verdoso de buena intensidad. Aroma a durazno blanco, damasco, hierbas frescas y algunas notas cítricas como limón y pomelo. En el sabor se distinguen una entrada dulce, notas cítricas, con un buen equilibrio entre la acidez y el azúcar natural.'
+    },
+    {
+        id: '14',
+        img: 'https://i.ibb.co/HrRqYJt/chac-chac-sauvignon-blanc.jpg',
+        nombre: 'CHAC CHAC SAUVIGNON BLANC',
+        bodega: 'las perdices',
+        precio: 990,
+        descripcion: 'Color amarillo verdoso muy sutil, notas de espárrago, ruda, pomelo rosado, con sutiles notas minerales. En boca, buen ataque, acidez marcada y final prolongado, remarcando las notas típicas de la variedad.'
+    },
+    {
+        id: '15',
+        img: 'https://i.ibb.co/BwCv77v/finca-gabriel-chardonnay.jpg',
+        nombre: 'FINCA GABRIEL CHARDONNAY',
+        bodega: 'jorge rubio',
+        precio: 638,
+        descripcion: 'Color amarillo con tonos verdes muy vivaces. Aroma intenso que recuerda a bananas y ananá. Sabor fresco con entrada dulce en boca.'
+    },
 ];
 
 export default Items;
