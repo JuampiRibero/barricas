6583da8c7de98ca8214b4b64d0f57090ad464ce6
JuampiRibero <juampiribero@gmail.com>
2022-01-15 10:42:28 -0300

    entrega practicos clase 5

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -6,56 +6,32 @@ export const useCartContext = () => useContext(CartContext);
 
 export const CartProvider = ({ children }) => {
     const [cart, setCart] = useState([]);
-    const [quantityInCart, setQuantityInCart] = useState(0);
 
     const addItem = (item, quantity) => {
-        const index = isInCart(item.id);
-
-        if(index === -1) {
-            cart.push({ item: item, quantity: quantity });
-            setQuantityInCart(quantityInCart + quantity);
-        } else {
+        const index = cart.findIndex((prod) => prod.item.id === item.id);
+        
+        if(index > -1) {
             const currentQuantity = cart[index].quantity;
-            if(currentQuantity + quantity <= item.stock) {
-                cart[index] = { item: item, quantity: currentQuantity + quantity };
-                setQuantityInCart(quantityInCart + quantity);
-            };
+            cart.splice(index, 1);
+            setCart([...cart, { item, quantity: quantity + currentQuantity }]);
+        } else {
+            setCart([...cart, { item, quantity }]);
         };
     };
 
-    const removeItem = (itemId) => {
-        const index = isInCart(itemId);
-    
-        if(index > -1) {
-            const quantity = cart[index].quantity;
-            setQuantityInCart(quantityInCart - quantity);
-            cart.splice(index, 1);
-        };
+    const removeItem = (id) => {
+        const deleteProduct = cart.filter((prod) => prod.item.id !== id);
+        setCart([...deleteProduct]);
     };
 
     const clearCart = () => {
         setCart([]);
-        setQuantityInCart(0);
-    };
-
-    const isInCart = (itemId) => {
-        let i = 0;
-        const length = cart.length;
-        let index = -1;
-        while (i < length && index === -1) {
-            if(cart[i].item.id === itemId) {
-                index = i;
-            } else {
-                i++;
-            }
-        };
-        return index;
     };
 
     console.log(cart);
 
     return (
-        <CartContext.Provider value={{ cart, setCart, quantityInCart, setQuantityInCart, addItem, removeItem, clearCart, isInCart }}>
+        <CartContext.Provider value={{ cart, setCart, addItem, removeItem, clearCart }}>
             { children }
         </CartContext.Provider>
     );
